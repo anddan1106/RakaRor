c6123840c8f417accb84fec1d31355b78da8b6e1
ACADEMY-6162X1V\Dennis Fosse <dennis.fosse@gmail.com>
2016-08-24 14:14:14 +0200

    Har lagt in några Scripts

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class MoveForward : MonoBehaviour
+    {
+        public float maxSpeed = 5f;
+        void Update()
+        {
+            Vector3 pos = transform.position;
+            Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+
+            pos += transform.rotation * velocity;
+            transform.position = pos;
+        }
+    }
+}
