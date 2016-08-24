812ba0d9ce6bba23cf3b6ab392688bce0c7544f8
ACADEMY-6162X1V\Dennis Fosse <dennis.fosse@gmail.com>
2016-08-24 17:04:51 +0200

    asd

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -6,10 +6,15 @@ namespace Assets.Scripts
     public class MoveForward : MonoBehaviour
     {
         public float maxSpeed = 5f;
+
+        void Start()
+        {
+            transform.Rotate(0, 0, 90);
+        }
         void Update()
         {
             Vector3 pos = transform.position;
-            Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+            Vector3 velocity = new Vector3(maxSpeed * Time.deltaTime, 0,  0);
 
             pos += transform.rotation * velocity;
             transform.position = pos;
