8aaf25ceebdbc2271de33a46d5a01cea2f0c142d
anddan1106 <anddan1106@gmail.com>
2016-08-25 13:44:41 +0200

    player moving animations done

---
+++ Assets/Scripts/SetWalkingDirection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetWalkingDirection : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = this.GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal != 0 && vertical == 0)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (vertical != 0)
+        {
+            animator.SetInteger("Direction", 1);
+        }
+        else
+        {
+            animator.SetInteger("Direction", 0);
+        }
+    }
+}
+
+
