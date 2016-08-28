f157aeea22d3215ed190f307a809e036116dbe33
anddan1106 <anddan1106@gmail.com>
2016-08-28 21:58:30 +0200

    minor changes to  walking sound and animation

--- Assets/Scripts/WalkingSound.cs
+++ Assets/Scripts/WalkingSound.cs
@@ -9,13 +9,13 @@ public class WalkingSound : MonoBehaviour
     //public AudioClip strafeSound;
     public AudioClip[] sounds;
     public GameObject AudioSource;
-    private AudioSource walkingSource;
+    private AudioSource walkingSoundSource;
 
     // Use this for initialization
     void Start()
     {
-        walkingSource = AudioSource.GetComponent<AudioSource>();
-        walkingSource.enabled = true;
+        walkingSoundSource = AudioSource.GetComponent<AudioSource>();
+        walkingSoundSource.enabled = true;
 
     }
 
@@ -26,15 +26,15 @@ public class WalkingSound : MonoBehaviour
         var vertical = Input.GetAxis("Vertical");
         var horizontal = Input.GetAxis("Horizontal");
 
-        if (horizontal != 0 && vertical == 0)
+        if (!walkingSoundSource.isPlaying)
         {
-            if (!walkingSource.isPlaying)
-                walkingSource.PlayOneShot(sounds[1]);
+            if (GetComponent<Rigidbody2D>().velocity.magnitude >= 0.2)
+                if (horizontal != 0 && vertical == 0)
+                    walkingSoundSource.PlayOneShot(sounds[1]);
+                else if (vertical != 0)
+                    walkingSoundSource.PlayOneShot(sounds[0]);
         }
-        else if (vertical != 0)
-        {
-            if (!walkingSource.isPlaying)
-                walkingSource.PlayOneShot(sounds[0]);
-        }
+
     }
 }
+
